53544b628fec2d42fc36a4cf3f6119f3e9b1d0ab
jamshid <jamshidalimov3464@gmail.com>
2023-02-13 16:25:40 +0500

    tugagan edi

--- app/src/main/java/com/chinthanrk/readexcel/MainActivity.java
+++ app/src/main/java/com/chinthanrk/readexcel/MainActivity.java
@@ -1,37 +1,30 @@
 package com.chinthanrk.readexcel;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.view.MenuItemCompat;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Switch;
-import android.widget.TextView;
-
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    ArrayAdapter<String > adapter;
     public static String [] name=new String[152];
     public static String [] mano=new String[152];
     public static String [] qollanish=new String[152];
@@ -70,17 +63,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        ArrayAdapter<String>adapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,name);
+        adapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,name);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                aka=mano[i];
-                uka=qollanish[i];
-                dialog.setContentView(R.layout.play);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            aka=mano[i];
+            uka=qollanish[i];
+            dialog.setContentView(R.layout.play);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.show();
         });
     }
     public void clickmano(View view){
@@ -88,5 +78,29 @@ public class MainActivity extends AppCompatActivity {
     }
     public void clickqollanish(View view){
         startActivity(new Intent(MainActivity.this,qollanishi.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
+        final SearchView searchView;
+        searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/chinthanrk/readexcel/Manosi.java
+++ app/src/main/java/com/chinthanrk/readexcel/Manosi.java
@@ -1,7 +1,5 @@
 package com.chinthanrk.readexcel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
